cea3b55bc70756ef8eaf042e584fbcfa8cf5a4ac
Maria Fernanda Montezuma  Gonzalez <maria.montezuma@globant.com>
2025-10-28 06:21:39 -0500

    se crea proyecto

---
+++ src/test/java/com/example/arepasDonLucho/ArepasDonLuchoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arepasDonLucho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArepasDonLuchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
